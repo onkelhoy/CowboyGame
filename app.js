6c5379ac372816d76b2d7aae2b48f92c84386347
Henry Pap <onkelhoy@gmail.com>
2016-03-24 12:12:25 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var game = require('./GameServer')(io);
+
+
+app.use('/', express.static(__dirname + '/public'));
+app.get('/', function(req, res){
+	res.sendfile('html/create.html');
+});
+app.get('/game', function(req, res){
+	res.sendfile('html/game.html');
+});
+app.get('/gameLoss', function(req, res){
+	res.sendfile('html/gameLoss.html');
+});
+app.get('*', function(req, res){
+	res.redirect('/');
+});
+
+
+server.listen(3000);
